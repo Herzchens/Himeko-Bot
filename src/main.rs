a1660e2bb8a13fca95df79b9391bd333cf14947d
Itztli Herzchen <166841132+Herzchens@users.noreply.github.com>
2026-04-30 20:18:12 +0700

    refactor(phase-1): update project name to Himeko-Bot and clean up stale files

--- src/main.rs
+++ src/main.rs
@@ -36,7 +36,7 @@ async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt()
         .with_env_filter(
             tracing_subscriber::EnvFilter::from_default_env()
-                .add_directive("tts_bot=debug".parse()?),
+                .add_directive("himeko_bot=debug".parse()?),
         )
         .init();
 
